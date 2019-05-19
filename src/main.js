a7fc58b0f4c89fc4e626a169925d1a6e5f098156
Kanae Matsuguchi <kmatsuguchi@Kanaes-MacBook-Air.local>
2019-05-19 13:58:34 +0900

    first version of screens

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
+//import './plugins/axios'
+import router from './router'
+import './plugins/vuetify'
 import App from './App.vue'
+
+Vue.use(router)
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
